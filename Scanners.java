0e368216add996276857e2b6bdedafdc02062f6e
Raghav Singh <actualrag@gmail.com>
2023-10-20 14:49:07 +0530

    Update w code

---
+++ Scanners.java
@@ -0,0 +1,151 @@
+import java.util.*;
+public class Scanners {
+    public static void main(String[]args ) {
+
+//        System.out.println("Type the first value below: ");
+//        Scanner sc = new Scanner(System.in);
+//        Integer a = sc.nextInt();
+//        System.out.println("Type in your second number: ");
+//        Integer b = sc.nextInt();
+//        int sum = a+b;
+//        System.out.println("your answer is: " + sum);
+
+//        System.out.println("What is your age? ");
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//        if (age >= 18){
+//            System.out.println("You are an adult!");
+//        }
+//        else {
+//            System.out.println("Not adult!");
+//        }
+
+//        System.out.println("Write your number below: ");
+//        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//        if (x % 2 == 0){
+//
+//            System.out.println("This is an even number!");
+//
+//        } else{
+//
+//            System.out.println("This is an odd number!");
+//
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Type your first number: ");
+//        int a = sc.nextInt();
+//        System.out.println("Type your second numjber: ");
+//        int b = sc.nextInt();
+//        if (a > b){
+//            System.out.println("A is greater than B");
+//
+//        } else if (a < b) {
+//
+//            System.out.println("A is smaller than B");
+//
+//
+//        } else if (a == b) {
+//
+//            System.out.println("A is equal of B");
+//
+//
+//        } else {
+//            System.out.println("Error");
+//        }
+//        }
+//
+//
+//    }
+
+//        Scanner sc = new Scanner(System.in);
+//        int button = sc.nextInt();
+//        if (button == 1){
+//            System.out.println("Hello");
+//        } else if (button == 2) {
+//            System.out.println("Namaste");
+//        } else if (button == 3) {
+//            System.out.println("Bonjour");
+//
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        int button = sc.nextInt();
+//        switch (button){
+//            case 1:
+//                System.out.println("Hello");
+//            break;
+//            case 2:
+//                System.out.println("Namaste");
+//            break;
+//            case 3:
+//                System.out.println("Bonjour");
+//            break;
+//            default:
+//                System.out.println("Invalid button");
+//        }
+
+        // HW 1 Calculator [Multiplications]
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Number multiplyer");
+//        System.out.println("Type in number one below: ");
+//        int a = sc.nextInt();
+//        System.out.println("Type in number two below: ");
+//        int b = sc.nextInt();
+//        int mul = (a * b);
+//        System.out.println("Your answer is " + mul);
+
+        // HW 2 Month - Number gen
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Number - Month Finder");
+//        System.out.println("Enter the number of the month you want to find [Between 1-12] below: ");
+//        int mon = sc.nextInt();
+//        switch (mon){
+//            case 1:
+//                System.out.println("January");
+//            break;
+//            case 2:
+//                System.out.println("Feburary");
+//            break;
+//            case 3:
+//                System.out.println("March");
+//            break;
+//            case 4:
+//                System.out.println("April");
+//            break;
+//            case 5:
+//                System.out.println("May");
+//            break;
+//            case 6:
+//                System.out.println("June");
+//            break;
+//            case 7:
+//                System.out.println("July");
+//            break;
+//            case 8:
+//                System.out.println("August");
+//            break;
+//            case 9:
+//                System.out.println("September");
+//            break;
+//            case 10:
+//                System.out.println("October");
+//            break;
+//            case 11:
+//                System.out.println("November");
+//            break;
+//            case 12:
+//                System.out.println("December");
+//            break;
+//            default:
+//                System.out.println("Invalid month number!");
+//
+//
+//        }
+
+        }
+
+    }
